b9ed3e93a97fa26e1f7fac99940758259b3c2d4e
Emmanuel_Akintunde <tunde@reach.africa>
2018-11-10 14:25:01 +0100

    Another Commit

--- app/src/main/java/com/example/android/shushme/GeoFencingBroadcastReceiver.java
+++ app/src/main/java/com/example/android/shushme/GeoFencingBroadcastReceiver.java
@@ -1,12 +1,119 @@
 package com.example.android.shushme;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.os.Build;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
 
 public class GeoFencingBroadcastReceiver extends BroadcastReceiver {
+
+    private final String TAG = GeoFencingBroadcastReceiver.class.getSimpleName();
+    private static final int GEOFENCE_PENDING_INTENT_REQUEST_CODE = 0;
+    private int ringerMode;
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        GeofencingEvent event = GeofencingEvent.fromIntent(intent);
+
+        //check if the triggering has no error
+        if (event.hasError()) {
+            Log.d(TAG, String.format("Error occurred: %s", event.getErrorCode()));
+            return;
+        }
+
+        //know the transition that triggered this event
+        int transitionType = event.getGeofenceTransition();
+        switch (transitionType) {
+
+            case Geofence.GEOFENCE_TRANSITION_ENTER:
+                setRingerMode(context, transitionType);
+                break;
+
+            case Geofence.GEOFENCE_TRANSITION_EXIT:
+                setRingerMode(context, transitionType);
+                break;
+        }
+
+        sendNotification(context, transitionType);
+    }
+
+    public void setRingerMode(Context context, int ringerMode) {
+
+        NotificationManager notifManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N
+                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && !notifManager.isNotificationPolicyAccessGranted())) {
+            AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            audioManager.setRingerMode(ringerMode);
+        }
+    }
+
+    private void sendNotification(Context context, int transitionType) {
+
+        Intent intent = new Intent(context, MainActivity.class);
+
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+        taskStackBuilder.addParentStack(MainActivity.class);
+        taskStackBuilder.addNextIntent(intent);
+
+        PendingIntent contentIntent = taskStackBuilder.getPendingIntent(GEOFENCE_PENDING_INTENT_REQUEST_CODE, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Create an explicit content Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+
+        // Construct a task stack.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(MainActivity.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
+
+        // Get a PendingIntent containing the entire back stack.
+        PendingIntent notificationPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Get a notification builder
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        // Check the transition type to display the relevant icon image
+        if (transitionType == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            builder.setSmallIcon(R.drawable.ic_volume_off_white_24dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
+                            R.drawable.ic_volume_off_white_24dp))
+                    .setContentTitle(context.getString(R.string.silent_mode_activated));
+        } else if (transitionType == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            builder.setSmallIcon(R.drawable.ic_volume_up_white_24dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
+                            R.drawable.ic_volume_up_white_24dp))
+                    .setContentTitle(context.getString(R.string.back_to_normal));
+        }
+
+        // Continue building the notification
+        builder.setContentText(context.getString(R.string.touch_to_relaunch));
+        builder.setContentIntent(notificationPendingIntent);
+
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true);
+
+        // Get an instance of the Notification manager
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build());
     }
 }
+
